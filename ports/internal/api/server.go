e1320337532b2f90cadef879fabc993bca5ef399
ozoniuss <alexbledea_2000@yahoo.com>
2023-03-05 17:48:35 +0200

    feat(ports/api): initialize port service

--- ports/internal/api/server.go
+++ ports/internal/api/server.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"fmt"
+
 	pb "github.com/Ozoniuss/golang-microservices-demo/protobuf/ports/api"
 
 	cfg "github.com/Ozoniuss/golang-microservices-demo/ports/internal/config"
 	"github.com/Ozoniuss/golang-microservices-demo/ports/internal/store"
 	"github.com/Ozoniuss/golang-microservices-demo/ports/internal/store/inmemory"
+	"github.com/Ozoniuss/golang-microservices-demo/ports/internal/store/postgres"
 )
 
 type PortService struct {
@@ -15,19 +18,22 @@ type PortService struct {
 
 func NewPortsService(config cfg.Config) (*PortService, error) {
 
+	var storage store.Store
+	var err error
+
 	if config.Database.Inmemory {
-		storage := inmemory.NewPortsDatabase()
+		storage = inmemory.NewPortsDatabase()
 		return &PortService{
 			stoarge: storage,
 		}, nil
+	} else {
+		storage, err = postgres.NewPortsDatabase(config.Database)
+		if err != nil {
+			return nil, fmt.Errorf("could not initialize ports postgres database: %w", err)
+		}
 	}
 
-	// db, err := database.Connect(config.Database)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("could not connect to database: %w", err)
-	// }
-	// return &PortService{
-	// 	stoarge: db,
-	// }, nil
-	return nil, nil
+	return &PortService{
+		stoarge: storage,
+	}, nil
 }
